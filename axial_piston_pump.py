2bf1a6bb464b991b22cbfdb42dd3d58e1e0008fb
FMoroni <n286654@flow-04.ing.unimo.it>
2024-05-02 12:02:31 +0200

    trial

--- axial_piston_pump.py
+++ axial_piston_pump.py
@@ -17,9 +17,11 @@ A_max = 90.0e-6  # maximum area of suction and delivery ports [m^2]
 pi    = np.pi    # greek pi
 ntot  = 720      # total number of discretization points for the angular interval
 temp  = 0.0      # temporary variable for theta calculation
-omega = 3000     # rpm of the shaft of the pump
 
+omega = 3000     # rpm of the shaft of the pump
 omega = 2.0*pi*omega/60.0  # conversion to radiants [rad/s]
+
+cd = 0.7         # discharge coefficient (same for delivery and suction)
 
 # Variables
 theta  = np.linspace(0.0,2.0*pi,ntot)  # theta angle of rotation in radiants
@@ -47,8 +49,7 @@ for j in range(0,ntot):
         asuct[j] = A_max
     elif temp >= 11.0/6.0*pi and temp <= 2.0*pi:
         asuct[j] = - A_max/(pi/6.0) * j * dtheta + 12.0*A_max
-        
-              
+                     
 # Plot of delivery and suction areas
 plt.scatter(theta, adeliv, label="delivery") 
 plt.scatter(theta, asuct, label="suction")
@@ -61,6 +62,8 @@ labels = ["$0$", r"$\pi$", "$2 \pi$"]
 plt.xticks([0, pi, 2.0*pi], labels, color="k", size=20, rotation='horizontal')
 plt.legend(loc="upper left", fontsize=16)
 plt.show()
+
+# Volume
 
 
 
