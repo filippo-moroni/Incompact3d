9a5eb8646167c5b5c854b839a7c2403916523276
FMoroni <n286654@flow-04.ing.unimo.it>
2024-05-08 12:23:52 +0200

    trial

--- 2-solver/src/xcompact3d.f90
+++ 2-solver/src/xcompact3d.f90
@@ -317,11 +317,11 @@ subroutine finalise_xcompact3d()
       inquire(file="dt_history.txt", exist=exists)
       if (exists) then
           open(newunit=iunit, file="dt_history.txt", status="old", position="append", action="write")
-          write(iunit, *)  dt,  ',',  ifirst,  ',',  ilast
+          write(iunit, '(F8.6,A,I12,A,I12)')  dt,  ',',  ifirst,  ',',  ilast
       else
           open(newunit=iunit, file="dt_history.txt", status="new", action="write")
-          write(iunit, *) 'dt', ',', 'ifirst', ',', 'ilast'
-          write(iunit, *)  dt,  ',',  ifirst,  ',',  ilast
+          write(iunit, '(A8,A,A12,A,A12)') 'dt', ',', 'ifirst', ',', 'ilast'
+          write(iunit, '(F8.6,A,I12,A,I12)')  dt,  ',',  ifirst,  ',',  ilast
       end if
       close(iunit)
   end if
