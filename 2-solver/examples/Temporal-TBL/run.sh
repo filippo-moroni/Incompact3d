130082c780dca83ebb8ded6ff983d182508dad58
FMoroni <n286654@flow-04.ing.unimo.it>
2024-05-22 15:05:50 +0200

    trial

---
+++ 2-solver/examples/Temporal-TBL/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=fluids       # account name
+#SBATCH --partition=high       # partition name
+#SBATCH --time=23:59:59        # format HH : MM : SS
+#SBATCH --nodes=1              # node number
+#SBATCH --ntasks-per-node=52   # tasks out of 52
+#SBATCH --mem=400 g            # memory per node
+#SBATCH --exclusive            # don’t share nodes
+#SBATCH --job-name=test_TTBL   # job name
+#------------------- End of SLURM directives -----------------#
+
+# Loading of modules
+module purge
+module load gcc-12.1.1/gcc
+module load gcc-12.1.1/ompi-4.1.5_nccl
+
+# Launching
+mpirun -np 52 ../../build/bin/xcompact3d > out &
