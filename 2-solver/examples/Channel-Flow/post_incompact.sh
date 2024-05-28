ad33ca61dffed497575321c9b2ec146cb33ffa81
FMoroni <n286654@flow-04.ing.unimo.it>
2024-05-28 09:09:27 +0200

    trial

--- 2-solver/examples/Channel-Flow/post_incompact.sh
+++ 2-solver/examples/Channel-Flow/post_incompact.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account=fluids       # account name
-#SBATCH --partition=high       # partition name
-#SBATCH --time=23:59:59        # format HH : MM : SS
-#SBATCH --nodes=1              # node number
-#SBATCH --ntasks-per-node=52   # tasks out of 52
-#SBATCH --mem=400g             # memory per node
-#SBATCH --exclusive            # do not share nodes
-#SBATCH --job-name=test_TTBL   # job name
+#SBATCH --account=fluids          # account name
+#SBATCH --partition=high          # partition name
+#SBATCH --time=23:59:59           # format HH : MM : SS
+#SBATCH --nodes=1                 # node number
+#SBATCH --ntasks-per-node=52      # tasks out of 52
+#SBATCH --mem=400g                # memory per node
+#SBATCH --exclusive               # do not share nodes
+#SBATCH --job-name=test_Channel   # job name
 #------------------- End of SLURM directives -----------------#
 
 # Loading of modules
