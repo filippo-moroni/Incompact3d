618529340c60bdb54200a68b5c773389a0a6b98c
FMoroni <n286654@flow-04.ing.unimo.it>
2024-06-04 09:47:24 +0200

    trial

---
+++ 2-solver/examples/Channel-Flow - Re_tau = 200 - CFR/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=fluids        # account name
+#SBATCH --partition=high        # partition name
+#SBATCH --time=23:59:59         # format HH : MM : SS
+#SBATCH --nodes=1               # node number
+#SBATCH --ntasks-per-node=52    # tasks out of 52
+#SBATCH --mem=400g              # memory per node
+#SBATCH --exclusive             # do not share nodes
+#SBATCH --job-name=test_Channel # job name
+#------------------- End of SLURM directives -----------------#
+
+# Loading of modules
+module load gcc-12.1.1/gcc
+module load gcc-12.1.1/ompi-4.1.5_nccl
+
+# Launching
+mpirun -np 52 ../../build/bin/xcompact3d > out
