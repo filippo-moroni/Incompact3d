f24295adce8c83096837489db094686f830013e4
FMoroni <n286654@flow-04.ing.unimo.it>
2024-06-21 09:46:52 +0200

    trial

--- 2-solver/examples/Channel-Flow_retau200_cfr/post_incompact.sh
+++ 2-solver/examples/Channel-Flow_retau200_cfr/post_incompact.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test_Channel # job name
-#SBATCH --account=fluids        # account name
-#SBATCH --partition=high        # partition name
-#SBATCH --time=23:59:59         # format HH : MM : SS
-#SBATCH --nodes=1               # node number
-#SBATCH --ntasks-per-node=52    # tasks out of 52
-#SBATCH --cpus-per-task=1       # CPUs per task
-#SBATCH --mem=400g              # memory per node
-#SBATCH --exclusive             # exclusive use of the nodes
+#SBATCH --job-name=test           # job name
+#SBATCH --account=fluids          # account name
+#SBATCH --partition=high          # partition name
+#SBATCH --time=23:59:59           # format HH : MM : SS
+#SBATCH --nodes=1                 # node number
+#SBATCH --ntasks-per-node=52      # tasks out of 52
+#SBATCH --cpus-per-task=1         # CPUs per task
+#SBATCH --mem=400g                # memory per node
+#SBATCH --exclusive               # exclusive use of the nodes
+#SBATCH --exclude=cnode[06,17,19] # to exclude some nodes
 #------------------- End of SLURM directives -----------------#
 
 # Loading of modules
