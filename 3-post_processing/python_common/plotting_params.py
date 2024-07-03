0c23e87b842edb2c493e0e8c7446c75379d1bbde
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-03 15:57:33 +0200

    trial

---
+++ 3-post_processing/python_common/plotting_params.py
@@ -0,0 +1,51 @@
+#!-------------------------------------------------------------!
+#! This is a small module to store Python plotting parameters. !
+#!-------------------------------------------------------------!
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Settings
+np.seterr(divide='ignore', invalid='ignore')
+
+plt.rcParams.update({ 
+    "text.usetex": True,  
+    "font.family": "serif",
+    "font.sans-serif": "Computer Modern",
+    "figure.autolayout": True,
+})
+
+# Parameters for plotting
+lw           = 0.6             # linewidth for plots
+markersize   = 8.0             # marker size for scatter plot
+fla          = 10              # fontsize of labels of x and y axes (major labels, variables)
+fla2         = 4.5             # fontsize of numbers of x and y axes 
+pad_axes_lab = 2               # padding of axes labels
+pad_numbers  = 3               # padding of numbers on both axes
+lmajt        = 4               # length of major ticks
+lmint        = 2               # length of minor ticks
+tick_width   = 0.5             # width of ticks and external box
+xliminf      = 0.1             # x axis inferior limit (y+)
+
+# Page settings (A4 paper format: 8.3 x 11.7 inches)
+xinches      = 2.6             # size in inches in x direction of the image
+yinches      = 2.2             # size in inches in y direction of the image
+
+# Axes width
+mpl.rcParams['axes.linewidth'] = tick_width
+
+# Set some useful colors
+grey = [0.5, 0.5, 0.5]
+
+# Parameter to switch between Lee & Moser reference or Cimarelli, 'Turbulence' lecture notes for the log law
+iswitch = 1 # (0: Lee & Moser, 1: Cimarelli)
+
+# Column width for writing to .txt file
+c_w = 16 
+
+# Format for numbers
+fs = f"<{c_w}.3f"
+
+# Format for cf only
+fs2 = f"<{c_w}.8f"
