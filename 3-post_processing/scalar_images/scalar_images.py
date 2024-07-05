b795abb40fac4cd8f316793991b9e830e15c7d76
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-05 11:35:58 +0200

    trial

--- 3-post_processing/scalar_images/scalar_images.py
+++ 3-post_processing/scalar_images/scalar_images.py
@@ -39,6 +39,19 @@ itype, nx, ny, nz, Lx, Ly, Lz, re, iswitch_wo, file1, filen, icrfile, nr, add_st
 
 #!--------------------------------------------------------------------------------------!
 
+# Settings
+
+# Limits for axes (not used in reality, but needed to use 'set_plot_settings'
+xliminf = 0.0
+xlimsup = 1.0
+yliminf = 0.0
+ylimsup = 1.0
+
+# Extent of the image (dimensions of the domain)
+extent = [0.0, Lx, 0.0, Ly]
+
+#!--------------------------------------------------------------------------------------!
+
 #!--- Mesh section ---!
 
 # Create x-coordinates vector
@@ -52,60 +65,78 @@ X, Y = np.meshgrid(x, y)
 
 #!--------------------------------------------------------------------------------------!
 
-# Define the binary file path
-file_path = 'data/planes/phiplanez-0059.bin'
+# Use first realization folder to read planes if we have more than 1 realization
+if nr != 1:
+    # /data_ri folders if nr /= 1
+    file_path = f"data_r{nr:01d}/planes"
+
+# Else use just /data folder       
+elif nr == 1:
+    # /data folder if nr = 1
+    file_path = f"data/planes"
+
+# Cycle on all planes
+i = 0
+while True:
+
+    # Create the file path for scalar planes
+    file_path = file_path + f'/phiplanez-{i:04d}.bin' 
+    
+    # Exit the loop if the file does not exist
+    if not os.path.exists(file_path):
+        break  
+    
+    # Read the instantaneous scalar field binary file into a numpy array
+    with open(file_path, 'rb') as file:
+        data = np.fromfile(file, dtype=np.float64)
+
+    # Reshape scalar field to 2D array using Fortran order
+    data = data.reshape((nx, ny), order='F')
+
+    # Transpose and flip upside-down
+    data = np.flipud(data.T)
+
+    # Subplots environment
+    fig, ax = plt.subplots(1, 1, figsize=(pp.xinches,pp.yinches), linewidth=pp.tick_width, dpi=300)
+
+    # Plotting the image
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='10%', pad=0.05)
+    im = ax.imshow(data, cmap='Blues', extent=extent, vmin=0.0, vmax=1.0)
+
+    # Colorbar
+    cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+
+    # Colorbar ticks 
+    cbar.ax.tick_params(axis='y', labelsize=pp.fla2, length=pp.lmajt, width=pp.tick_width) 
+
+    # Colorbar label
+    cbar.set_label(r'$\varphi/\varphi_w$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)  
+
+    # Axes labels
+    ax.set_xlabel(r'$x/D$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)
+    ax.set_ylabel(r'$y/D$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)
+
+    # Set the plot parameters using the function 'set_plot_settings'
+    # Last argument is the switcher for semilog plot (1: yes, 0: no)
+    set_plot_settings(ax, xliminf, xlimsup, yliminf, ylimsup, pp, 0)
+
+    # Specify manually the interval for xticks
+    major_ticks_interval = 10.0
+    ax.xaxis.set_major_locator(MultipleLocator(major_ticks_interval))
+
+    # Saving the figure
+    plt.savefig(f'images/phiplanez_{add_string}.png', format='png', bbox_inches='tight', dpi=300)
+    
+    # Move to the next file index
+    i += 1  
+
+print(f"Reached file {i:04d} which does not exist. Stopping.")
 
 
-# Read the instantaneous scalar field binary file into a numpy array
-with open(file_path, 'rb') as file:
-    data = np.fromfile(file, dtype=np.float64)
 
-# Reshape scalar field to 2D array using Fortran order
-data = data.reshape((nx, ny), order='F')
 
-# Transpose and flip upside-down
-data = np.flipud(data.T)
 
-# Limits for axes (not used in reality, but needed to use 'set_plot_settings'
-xliminf = 0.0
-xlimsup = 1.0
-yliminf = 0.0
-ylimsup = 1.0
-
-# Subplots environment
-fig, ax = plt.subplots(1, 1, figsize=(pp.xinches,pp.yinches), linewidth=pp.tick_width, dpi=300)
-
-# Extent of the image (dimensions of the domain)
-extent = [0.0, Lx, 0.0, Ly]
-
-# Plotting the image
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='10%', pad=0.05)
-im = ax.imshow(data, cmap='Blues', extent=extent, vmin=0.0, vmax=1.0)
-
-# Colorbar
-cbar = fig.colorbar(im, cax=cax, orientation='vertical')
-
-# Colorbar ticks 
-cbar.ax.tick_params(axis='y', labelsize=pp.fla2, length=pp.lmajt, width=pp.tick_width) 
-
-# Colorbar label
-cbar.set_label(r'$\varphi/\varphi_w$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)  
-
-# Axes labels
-ax.set_xlabel(r'$x/D$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)
-ax.set_ylabel(r'$y/D$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)
-
-# Set the plot parameters using the function 'set_plot_settings'
-# Last argument is the switcher for semilog plot (1: yes, 0: no)
-set_plot_settings(ax, xliminf, xlimsup, yliminf, ylimsup, pp, 0)
-
-# Specify manually the interval for xticks
-major_ticks_interval = 10.0
-ax.xaxis.set_major_locator(MultipleLocator(major_ticks_interval))
-
-# Saving the figure
-plt.savefig(f'images/phiplanez_{add_string}.png', format='png', bbox_inches='tight', dpi=300)
     
 
 
