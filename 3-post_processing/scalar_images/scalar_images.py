7f904d344b96507e6f85ab3d49cec03798bb6a32
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-05 15:16:39 +0200

    trial

--- 3-post_processing/scalar_images/scalar_images.py
+++ 3-post_processing/scalar_images/scalar_images.py
@@ -118,7 +118,7 @@ while True:
     C = ax.contourf(x, y, data, lvls, cmap='Blues')
     
     # Colorbar
-    cbar = fig.colorbar(C, cax=cax, orientation='vertical', ticks=[0.0,1.0], reverse=True)
+    cbar = fig.colorbar(C, cax=cax, orientation='vertical', ticks=[0.0,1.0])
        
     # Colorbar ticks 
     cbar.ax.tick_params(axis='y', labelsize=pp.fla2, length=pp.lmajt, width=pp.tick_width) 
