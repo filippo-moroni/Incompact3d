7dbbbcde27eae0bae2aa79a07ea6dcc28c4f14c7
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-10 17:12:06 +0200

    trial

--- 3-post_processing/python_common/read_incompact3d_files.py
+++ 3-post_processing/python_common/read_incompact3d_files.py
@@ -26,7 +26,7 @@ def read_input_files(filename1,filename2):
         Ly         = lines[22]
         Lz         = lines[23]
         re         = lines[26]
-        iswitch_wo = lines[87]
+        iswitch_wo = lines[88]
     
         # Removing characters in front of the extracted strings and the comments:
         # 1) split: the string is split when the specified character is encountered; 
