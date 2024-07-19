4ab55268e8e36756c9f57753891d2a7572ca1b25
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-19 09:26:02 +0200

    trial

--- 2-solver/src/BC-Channel-flow.f90
+++ 2-solver/src/BC-Channel-flow.f90
@@ -101,11 +101,15 @@ contains
          phi1(:,xsize(2),:,:) = zero
        endif
     endif
-!
+
+    ! IC
     ux1=zero
     uy1=zero
     uz1=zero
-    byx1=zero;byy1=zero;byz1=zero
+    byx1=zero
+    byy1=zero
+    byz1=zero
+    
     ! if to decide type of initialization to apply 
     if (iin == 0) then ! laminar flow
        do k=1,xsize(3)
