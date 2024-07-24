106d099baf32b53df30997a64f9835fa4eb14116
FMoroni <n286654@flow-04.ing.unimo.it>
2024-07-24 09:51:48 +0200

    trial

--- 3-post_processing/field_images/field_images.py
+++ 3-post_processing/field_images/field_images.py
@@ -245,4 +245,3 @@ print(f"Reached file index {i:04d} which does not exist. Stopping.")
 #os.system("ffmpeg -y -framerate 30 -start_number 0 -i field_flowcasename_%04d.png field_flowcasename.mp4")
 
 
-
