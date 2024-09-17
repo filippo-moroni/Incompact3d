f8e6049b0711630b5c0e230cc698f42f4174fad7
filippo-moroni <121792527+filippo-moroni@users.noreply.github.com>
2024-09-17 12:11:58 +0200

    Update plot_statistics.py

--- 3-post_processing/plot_statistics/plot_statistics.py
+++ 3-post_processing/plot_statistics/plot_statistics.py
@@ -162,13 +162,13 @@ if post_vort:
     vort_y *= t_nu
     vort_z *= t_nu
 
-# Rescale TKE terms    
+# Rescale TKE terms (as Mansour et al. (1988))
 if post_tke_eq:
-    tke_turbt  /= sh_vel**2
-    tke_presst /= sh_vel**2
-    tke_difft  /= sh_vel**2
-    tke_prod   /= sh_vel**2
-    tke_pseps  /= sh_vel**2
+    tke_turbt  /= sh_vel**4 * nu
+    tke_presst /= sh_vel**4 * nu
+    tke_difft  /= sh_vel**4 * nu
+    tke_prod   /= sh_vel**4 * nu
+    tke_pseps  /= sh_vel**4 * nu
 
 # Friction Reynolds number
 re_tau = None
@@ -1009,11 +1009,11 @@ if post_tke_eq:
     ax.scatter(y_plus[:ny], -tke_pseps [:ny], marker='o', linewidth=pp.lw, s=pp.markersize, facecolors='none', edgecolors='C4')
 
     # Mansour et al. (1988)            
-    ax.plot(y_plus_tke_turbt_mansour,  tke_turbt_mansour,  color='C0', linestyle='-', linewidth=pp.lw)
-    ax.plot(y_plus_tke_presst_mansour, tke_presst_mansour, color='C1', linestyle='-', linewidth=pp.lw)
-    ax.plot(y_plus_tke_difft_mansour,  tke_difft_mansour,  color='C2', linestyle='-', linewidth=pp.lw)
-    ax.plot(y_plus_tke_prod_mansour,   tke_prod_mansour,   color='C3', linestyle='-', linewidth=pp.lw)
-    ax.plot(y_plus_tke_pseps_mansour,  tke_pseps_mansour,  color='C4', linestyle='-', linewidth=pp.lw)
+    ax.plot(y_plus_tke_turbt_mansour,  -tke_turbt_mansour,  color='C0', linestyle='-', linewidth=pp.lw)
+    ax.plot(y_plus_tke_presst_mansour, -tke_presst_mansour, color='C1', linestyle='-', linewidth=pp.lw)
+    ax.plot(y_plus_tke_difft_mansour,   tke_difft_mansour,  color='C2', linestyle='-', linewidth=pp.lw)
+    ax.plot(y_plus_tke_prod_mansour,    tke_prod_mansour,   color='C3', linestyle='-', linewidth=pp.lw)
+    ax.plot(y_plus_tke_pseps_mansour,   tke_pseps_mansour,  color='C4', linestyle='-', linewidth=pp.lw)
                    
     # Axes labels
     ax.set_xlabel(r'$y^+$', fontsize=pp.fla, labelpad=pp.pad_axes_lab)
