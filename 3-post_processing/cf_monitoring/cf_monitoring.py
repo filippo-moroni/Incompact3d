0cc99e25a3c1aba4aac8e35012ba3503046f526c
FMoroni <n286654@flow-04.ing.unimo.it>
2024-09-25 11:30:35 +0200

    trial

--- 3-post_processing/cf_monitoring/cf_monitoring.py
+++ 3-post_processing/cf_monitoring/cf_monitoring.py
@@ -508,7 +508,7 @@ if itype == 13:
     ax.plot(re_mom_t, re_disp_t, color='C0', linestyle='-', linewidth=pp.lw)
     
     # G. Boga (Cimarelli et al. (2024a)) 
-    ax.plot(retheta_gboga, disp_t_gboga, color='C1', linestyle='-', linewidth=pp.lw)
+    ax.plot(retheta_gboga, disp_t_gboga*re, color='C1', linestyle='-', linewidth=pp.lw)
     
     # Axes labels
     ax.set_xlabel(r'$Re_\theta$',   fontsize=pp.fla, labelpad=pp.pad_axes_lab)
