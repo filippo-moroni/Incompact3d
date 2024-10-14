d3bb4146b1ee362ae80236cce64bddb56240da02
filippo-moroni <121792527+filippo-moroni@users.noreply.github.com>
2024-10-14 09:26:11 +0200

    Update plot_statistics.py

--- 3-post_processing/plot_statistics/plot_statistics.py
+++ 3-post_processing/plot_statistics/plot_statistics.py
@@ -1178,6 +1178,12 @@ if post_tke_eq and i_switch_plot == False:
 
     # Residual of TKE (excluding unsteady term if present)
     tke_residual = -tke_turbt - tke_presst + tke_difft + tke_prod - tke_pseps
+
+    # Find the maximum of the residual and print it to terminal
+    max_tke_residual = np.max(tke_residual)
+
+    print(">>> Maximum of TKE residual in wall units: ", max_tke_residual)
+    print()
     
     # Description of .pdf file
     description = 'Budget terms for Turbulent Kinetic Energy (TKE) equation. Reference data Mansour et al. (1988).'
