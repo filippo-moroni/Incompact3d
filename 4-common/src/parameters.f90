95844b1b72a4a80feb856f899c3e05187acfe726
FMoroni <n286654@flow-04.ing.unimo.it>
2024-10-17 09:31:36 +0200

    trial

--- 4-common/src/parameters.f90
+++ 4-common/src/parameters.f90
@@ -294,18 +294,13 @@ subroutine parameter(input_i3d)
 
   ! Implicit y-diffusion
   if (iimplicit.ne.0) then
-     
-     ! Trial RK3 with implicit y-diffusion
-     if (itimescheme == 5) then  
-        if (nrank==0) write(*,*) "Warning : implicit y-diffusion is experimental with RK3.   "
-     endif
-          
+               
      ! O6SVV: Order-6th Spectral Vanishing Viscosity operator 
      ! (hyperviscous scheme for 2nd order derivative)
      if (isecondder==5) then     
         if (nrank==0) write(*,*) "Warning : implicit y-diffusion coupled with O6SVV is       "
         if (nrank==0) write(*,*) "          experimental and available only for Dirichlet    "
-        if (nrank==0) write(*,*) "          BCs on both boundaries.                          "     
+        if (nrank==0) write(*,*) "          BCs on both boundaries (ncly1 = nclyn = 2).      "     
      endif
      
      ! xcst: coefficient for the implicit y-diffusion terms (it involves viscosity and time-step)
